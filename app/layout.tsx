85766481f65da605c435f962ba1acfcb69329647
angelDova <Angelfcb01@yahoo.com>
2023-10-28 01:07:50 -0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-providers";
+import { ToastProvider } from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="dovajitsu-theme"
           >
+            <ToastProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
