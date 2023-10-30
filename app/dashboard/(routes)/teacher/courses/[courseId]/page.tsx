5d21ac5613a49d48adfe2af99d945db4198fe8fd
angelDova <Angelfcb01@yahoo.com>
2023-10-30 16:24:01 -0700

    update

--- app/dashboard/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/dashboard/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,7 +1,38 @@
-const CourseIdPage = ({ params }: { params: { courseId: string } }) => {
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields} / ${totalFields})`;
+
   return <div className="">Course Id: {params.courseId}</div>;
 };
 
 export default CourseIdPage;
-
-//2:10:00
