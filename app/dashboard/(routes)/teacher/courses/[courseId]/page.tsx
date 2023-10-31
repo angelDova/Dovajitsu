667e608252b1e8b69b1f8213f556febed807d694
angelDova <Angelfcb01@yahoo.com>
2023-10-31 16:22:48 -0700

    update

--- app/dashboard/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/dashboard/(routes)/teacher/courses/[courseId]/page.tsx
@@ -30,9 +30,27 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
 
-  const completionText = `(${completedFields} / ${totalFields})`;
+  const completionText = `(${completedFields}/${totalFields})`;
 
-  return <div className="">Course Id: {params.courseId}</div>;
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="felx flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course Setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className="">
+          <div className="flex items-center gap-x-2">
+            <h2 className="text-xl">Customize your course</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CourseIdPage;
