6ef37588e65a3a86943cde92f70d8a5fa47d5adb
angelDova <Angelfcb01@yahoo.com>
2023-11-05 19:45:06 -0800

    update

--- app/dashboard/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/dashboard/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -58,7 +58,7 @@ export const AttachmentForm = ({
           {!isEditing && (
             <>
               <PlusCircle className="h-4 w-4 mr-2" />
-              Add a file
+              Add file(s)
             </>
           )}
         </Button>
