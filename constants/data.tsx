bb58883950eb0c717c985b377930e7ca2043fde5
angelDova <Angelfcb01@yahoo.com>
2023-11-15 20:43:40 -0800

    update

---
+++ constants/data.tsx
@@ -0,0 +1,41 @@
+export const links = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Projects",
+    href: "/",
+  },
+  {
+    title: "Agency",
+    href: "/",
+  },
+  {
+    title: "Expertise",
+    href: "/",
+  },
+  {
+    title: "Contact",
+    href: "/",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Facebook",
+    href: "/",
+  },
+  {
+    title: "LinkedIn",
+    href: "/",
+  },
+  {
+    title: "Instagram",
+    href: "/",
+  },
+  {
+    title: "Twitter",
+    href: "/",
+  },
+];
