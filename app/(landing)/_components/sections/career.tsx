5452eb3880e44a43012b205902c3f16bf1bdf38b
angelDova <Angelfcb01@yahoo.com>
2024-01-06 21:04:15 -0800

    update

--- app/(landing)/_components/sections/career.tsx
+++ app/(landing)/_components/sections/career.tsx
@@ -2,7 +2,7 @@ const Career = () => {
   return (
     <div className="min-h-full flex flex-col bg-blue-600">
       <div className="flex flex-col items-center justify-center md:justify-start text-center text-white gap-y-8 flex-1 px-6 py-10">
-        This is the Career page
+        This is the Career page...
       </div>
     </div>
   );
